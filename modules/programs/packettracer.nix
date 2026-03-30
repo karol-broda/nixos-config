238e6db0f13e5d901294968532a1ac24a1c983b5
Karol Broda <karol.broda@titanom.com>
2026-03-30 13:42:16 +0200

    fix(modules/programs): skip packet tracer when source .deb is missing from nix store

--- modules/programs/packettracer.nix
+++ modules/programs/packettracer.nix
@@ -6,32 +6,44 @@
 }: let
   inherit (lib) mkIf mkEnableOption getExe;
   cfg = config.personal.programs.packettracer;
-  packetTracerEval = builtins.tryEval pkgs-old-working.ciscoPacketTracer8;
-  packetTracerPkg =
-    if packetTracerEval.success
-    then packetTracerEval.value
-    else null;
+
+  requiredSource = pkgs-old-working.requireFile {
+    name = "CiscoPacketTracer822_amd64_signed.deb";
+    hash = "sha256-bNK4iR35LSyti2/cR0gPwIneCFxPP+leuA1UUKKn9y0=";
+    url = "https://www.netacad.com";
+  };
+  sourceAvailable = builtins.pathExists (builtins.unsafeDiscardStringContext "${requiredSource}");
+
+  packetTracerPkg = pkgs-old-working.ciscoPacketTracer8;
 in {
   options.personal.programs.packettracer = {
     enable = mkEnableOption "cisco packet tracer (firejailed)";
   };
 
-  config = mkIf (cfg.enable && packetTracerEval.success) {
-    programs.firejail = {
-      enable = true;
+  config = mkIf cfg.enable (
+    if sourceAvailable
+    then {
+      programs.firejail = {
+        enable = true;
 
-      wrappedBinaries = {
-        packettracer8 = {
-          executable = getExe packetTracerPkg;
-          desktop = "${packetTracerPkg}/share/applications/cisco-pt8.desktop.desktop";
+        wrappedBinaries = {
+          packettracer8 = {
+            executable = getExe packetTracerPkg;
+            desktop = "${packetTracerPkg}/share/applications/cisco-pt8.desktop.desktop";
 
-          extraArgs = [
-            "--net=none"
-            "--noprofile"
-            ''--env=QT_STYLE_OVERRIDE=""''
-          ];
+            extraArgs = [
+              "--net=none"
+              "--noprofile"
+              ''--env=QT_STYLE_OVERRIDE=""''
+            ];
+          };
         };
       };
-    };
-  };
+    }
+    else {
+      warnings = [
+        "packettracer: CiscoPacketTracer822_amd64_signed.deb is not in the nix store, skipping. Download it from https://www.netacad.com and run: nix-prefetch-url file:///path/to/CiscoPacketTracer822_amd64_signed.deb"
+      ];
+    }
+  );
 }
